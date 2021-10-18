bd1869eb5eeb9eb3e6ce909a10cb2fd9c510037f
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-10-18 14:46:32 +0300

    re add tasks

--- callBcak.js
+++ callBcak.js
@@ -1,3 +1,4 @@
+// Task One
 function addOne(arr) {
   const newArr = [];
   arr.forEach(function (item, index) {
@@ -5,63 +6,81 @@ function addOne(arr) {
   });
   return newArr;
 }
+// End Task One
 
+// Task Two
 function addExclamation(arr) {
   arr.forEach(function (item, index) {
     arr[index] = item + "!";
   });
   return arr;
 }
+// End Task Two
 
+// Task Three
 function addOneByMap(arr) {
   arr.map(function (item, index) {
     arr[index] = item + 1;
   });
   return arr;
 }
+// End Task Three
 
+// Task Four
 function addQuestion(arr) {
   arr.map(function (item, index) {
     arr[index] = item + "?";
   });
   return arr;
 }
+// End Task Four
 
+// Task Five
 function forLoopTwoToThe(arr) {
   arr.forEach(function (item, index) {
     arr[index] = 2 ** item;
   });
   return arr;
 }
+// End Task Five
 
+// Task Six
 function typeNum(arr) {
   const newArr = arr.filter(function (item, index) {
     return typeof item == "number";
   });
   return newArr;
 }
+// End Task Six
 
+// Task Se7en
 function containsAnd(arr) {
   const newArr = arr.filter(function (item, index) {
     return item.includes("and");
   });
   return newArr;
 }
+// End Task Se7en
 
+// Task Eight
 function oddValues(arr) {
   const newArr = arr.filter(function (item, index) {
     return item % 2 != 0;
   });
   return newArr;
 }
+// End Task Eight
 
+// Task Nine
 function addValues(arr) {
   const sum = arr.reduce(function (acc, item, index) {
     return (acc += item);
   });
   return sum;
 }
+// End Task Nine
 
+// Task Ten
 function countNumberOfElements(arr) {
   const newArr = arr.reduce(function (acc) {
     acc++;
@@ -69,7 +88,11 @@ function countNumberOfElements(arr) {
   }, 0);
   return newArr;
 }
+// End Task Ten
 
+//**************************  Extra ***********************************
+
+// Extra One
 const items = [
   {
     item: "switch",
@@ -87,3 +110,22 @@ function addPurchases(arr) {
   }, 0);
   return newArr;
 }
+// End Extra One
+
+// Extra Two
+function sortNumbers(arr) {
+  const newArr = arr.sort(function (a, b) {
+    return a - b;
+  });
+  return newArr;
+}
+// End Extra Two
+
+// Extra Three
+function sortBackwards(arr) {
+  const newArr = arr.sort(function (a, b) {
+    return b - a;
+  });
+  return newArr;
+}
+// End Extra Three
